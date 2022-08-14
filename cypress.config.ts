74b8e65e4a663b04d5beef6fa0e9b16827bf7ab9
Benson Miakoun <bmiakoun@gmail.com>
2022-08-14 22:31:45 +0800

    Initial commit

---
+++ cypress.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  projectId: "7s5okt",
+  viewportHeight: 1000,
+  viewportWidth: 1280,
+
+  retries: {
+    runMode: 2,
+    openMode: 1,
+  },
+
+  env: {
+    apiUrl: "http://localhost:3001",
+    mobileViewportWidthBreakpoint: 414,
+    coverage: false,
+    codeCoverage: {
+      url: "http://localhost:3001/__coverage__",
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
---
+++ src/components/TransactionCreateStepTwo.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
+
+test("on initial render pay button is disabled", async () => {
+  render(<TransactionCreateStepTwo sender={{ id: "test" }} receiver={{ id: "test" }} />);
+
+  expect(await screen.findByRole("button", { name: /pay/i })).toBeDisabled();
+
+  userEvent.type(screen.getByPlaceholderText(/amount/i), "50");
+  userEvent.type(screen.getByPlaceholderText(/add a note/i), "dinner");
+
+  expect(await screen.findByRole("button", { name: /pay/i })).toBeEnabled();
+});
